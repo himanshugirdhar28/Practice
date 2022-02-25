ba5603cd58f96b536858c07bdd6f04956ec910f0
HIMANSHU <himanshugirdhar28@gmail.com>
2022-02-25 09:50:16 +0530

    Added solution - LeetHub

---
+++ Finding middle element in a linked list - GFG/finding-middle-element-in-a-linked-list.cpp
@@ -0,0 +1,94 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        int b=0;
+        int c;
+        Node* a=head;
+        while(a!=NULL)
+        {
+            b++;
+            a=a->next;
+        }
+        //cout<<b<<endl;
+        c=b/2;
+        //cout<<c<<endl;
+        b=0;
+        a=head;
+        while(b!=c)
+        {
+            b++;
+            a=a->next;
+            
+        }
+        return(a->data);
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

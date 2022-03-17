a44058493764238eae47d685a35c0ebe25634e3d
HIMANSHU <himanshugirdhar28@gmail.com>
2022-03-17 21:53:35 +0530

    Added solution - LeetHub

--- Pythagorean Triplet - GFG/pythagorean-triplet.cpp
+++ Pythagorean Triplet - GFG/pythagorean-triplet.cpp
@@ -11,7 +11,25 @@ public:
 	// Pythagorean triplet exists or not
 	bool checkTriplet(int arr[], int n) {
 	    // code here
-	    int i,j,y,z;
+	    int i,j,k;
+	    vector<int>v;
+	    sort(arr,arr+n);
+	    for(i=0;i<n;i++){
+	        v.push_back(pow(arr[i],2));
+	    }
+	    
+	    for(i=0;i<n-2;i++){
+	        for(j=i+1;j<n-1;j++){
+	            for(k=j+1;k<n;k++){
+	                if(pow(arr[i],2)+pow(arr[j],2)==pow(arr[k],2)){
+	                    return(1);
+	                    
+	                }
+	            }
+	        }
+	    }
+	    return(0);
+	    /*int i,j,y,z;
 	    j=0;
 	    if(n<3)
 	    {

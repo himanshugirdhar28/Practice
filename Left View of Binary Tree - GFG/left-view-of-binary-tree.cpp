0d60693c28997376c5b4e35d65aecd46324cb75d
HIMANSHU <himanshugirdhar28@gmail.com>
2022-05-09 15:38:38 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
@@ -127,201 +127,54 @@ struct Node
  */
 
 //Function to return a list containing elements of left view of the binary tree.
-/*vector<int>v;
-int x=0;
-int y=-1;
-int z=0;*/
-/*void find(Node *root, queue<int>& q, Node *a,vector<int>& v)
-{
-    if(root==NULL)
-    {
-        return;
-    }
-    if(root!=a)
-    {
-        q.push(root->data);
-    }
-    
-    find(root->left,q,a,v);
-    find(root->right,q,a,v);
-    if(q.size())
-    {
-        v.push_back(q.front());
-    }
-    while(q.size())
-    {
-        q.pop();
-    }
-}*/
-/*vector<int> height(Node *root,vector<int> &nv,int count)
-{
-    if(count=count)
-    {
-        
-    }
-    if(root==NULL)
-    {
-        nv.push_back(0);
-        return nv;
-    }
-
-    height(root->left,nv);
-    height(root->right,nv);
-    nv.push_back(root->data); 
-    return(nv);
-    
-}
-vector<int> leftv(Node *root,vector<int> &v)
-{
-    if (root==NULL)
-   {
-       return v;
-   }
-   v.push_back(root->data);
-   leftv(root->left,v);
-   if(root->right!=NULL && root->left!=NULL)
-   {
-       leftv(root->right->left,v);
-   }
-   else if(root->right!=NULL && root->left==NULL)
-   {
-       leftv(root->right,v);
-   }
-   return(v);
-}
-*/
 vector<int> leftView(Node *root)
 {
+   // Your code here
     vector<Node*>v;
-    vector<int>a;
     vector<int>x;
     int i=0;
-    int f=0,t=0;
     int j,b,h;
     if(root==NULL)
     {
         return x;
     }
-    a.push_back(root->data);
     v.push_back(root);
     while(1)
     {
         b=0;
-        t=0;
-        h=a.size();
+        h=v.size();
+        x.push_back(v[i]->data);
         for(j=i;j<h;j++)
         {
-            //cout<<a[j]<<endl;
-            if(a[j]!=0 && t==0)
-            {
-                x.push_back(a[j]);
-                t=1;
-            }
             if(v[j]==NULL || v[j]->left==NULL && v[j]->right==NULL)
             {
                 continue;
-                /*a.push_back(0);
-                v.push_back(NULL);
-                a.push_back(0);
-                v.push_back(NULL);*/
             }
             if(v[j]->left==NULL && v[j]->right!=NULL)
             {
                 b=1;
-                //a.push_back(0);
-                //v.push_back(NULL);
-                a.push_back(v[j]->right->data);
                 v.push_back(v[j]->right);
             }
             else if(v[j]->left!=NULL && v[j]->right==NULL)
             {
                 b=1;
-                a.push_back(v[j]->left->data);
                 v.push_back(v[j]->left);
-                //a.push_back(0);
-                //v.push_back(NULL);
             }
             else
             {
                 b=1;
-                a.push_back(v[j]->left->data);
                 v.push_back(v[j]->left);
-                a.push_back(v[j]->right->data);
                 v.push_back(v[j]->right);
             }
             
         }
-        //cout<<a.size()<<endl;
         if(b==0)
         {
             break;
         }
         i=h;
-        f++;
         
         
     }
     return x;
-   // Your code here
-   /*vector<int>nv;
-   vector<int>v;
-   count=0;
-   nv=height(root,nv,count);
-   reverse(nv.begin(),nv.end());
-   for(auto it=nv.begin();it!=nv.end();it++)
-   {
-       cout<<*it<<" ";
-   }
-   cout<<(nv.size())<<endl;
-   return(leftv(root,v));*/
-   
-   
-   
- /*  if(y==x+1 || z==0)
-   {
-     v.push_back(root->data);
-     x++;
-     y=0;
-   }
-   
-   //int x=0;
-   if(root->left!=NULL)
-   {
-       v=leftView(root->left);
-   }
-   else if(root->right!=NULL)
-   {
-       v=leftView(root->right);
-   }
-   else
-   {
-       z=1;
-       return(v);
-   }
-   if(z==1)
-   {
-      v=leftView(root->right);
-      y++;
-   }*/
-   /*stack<int>st;
-   vector<int>v;
-   if (root==NULL)
-   {
-       return v;
-   }
-        
-   leftView(root->left);
-   leftView(root->right);
-   //st.push(root->data);
-   cout<<root->data<<endl;
-   return(v);*/
-   /*Node *a=root;
-   queue<int>q;
-   vector<int>v;
-   v.push_back(root->data);
-   find(root, q,a,v);
-   return(v);*/
-   
-   
-   
 }

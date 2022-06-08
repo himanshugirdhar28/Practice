9d4090122dc7d367c7c0e166bf680cd0d68c1617
HIMANSHU <himanshugirdhar28@gmail.com>
2022-06-08 16:51:46 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -13,43 +13,31 @@ class Solution{
         int i=-1,j=1;
         long long int sum=0;
         int res=0;
-        vector<int>v;
-        v.push_back(a[0]);
         int m=pow(10,5);
         if(a[0]==k)
         {
             res=1;
         }
         vector<vector<int>>v1(m);
-        v1[abs(v[0])].push_back(0);
+        v1[abs(a[0])].push_back(0);
         for(i=1;i<n;i++)
         {
-            v.push_back(a[i]+v[i-1]);
-            if(v[i]==k)
+            a[i]=a[i]+a[i-1];
+            if(a[i]==k)
             {
                 res=max((i+1),res);
             }
-            /*if(i==1997)
-            {
-                cout<<v[i]<<" ";
-                break;
-            }*/
-            v1[abs(v[i])%m].push_back(i);
+            v1[abs(a[i])%m].push_back(i);
         }
-        /*for(auto it : v)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl*/
         for(i=0;i<n;i++)
         {
-            if(v1[abs(k+v[i])%m].size()!=0)
+            if(v1[abs(k+a[i])%m].size()!=0)
             {
-                for(j=v1[abs(k+v[i])%m].size()-1;j>-1;j--)
+                for(j=v1[abs(k+a[i])%m].size()-1;j>-1;j--)
                 {
-                    if(v[v1[abs(k+v[i])%m][j]]==k+v[i] && v1[abs(k+v[i])%m][j]>i)
+                    if(a[v1[abs(k+a[i])%m][j]]==k+a[i] && v1[abs(k+a[i])%m][j]>i)
                     {
-                        res=max(res,(v1[abs(k+v[i])%m][j]-i));
+                        res=max(res,(v1[abs(k+a[i])%m][j]-i));
                         break;
                     }
                 }

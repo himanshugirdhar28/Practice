c9ec91e622d430ccdcb6987819b17e8839b696be
HIMANSHU <himanshugirdhar28@gmail.com>
2022-11-19 23:38:49 +0530

    Time: 352 ms (28.91%), Space: 61.3 MB (29.25%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) 
+    int findDuplicate(vector<int>& l) 
     {
-        // int i;
-        // for(i=0;i<l.size()-1;i++)
-        // {
-        //     while(l[i]-1!=i && l[i]!=l[l[i]-1])
-        //     {
-        //         swap(l[i],l[l[i]-1]);
-        //     }
-        //     if(l[i]==l[l[i]-1] && l[i]-1!=i)
-        //     {
-        //         return(l[i]);
-        //     }
-        // }
-        // return(l[l.size()-1]);
-        int slow = nums[0];
-  int fast = nums[0];
-  do {
-    slow = nums[slow];
-    fast = nums[nums[fast]];
-  } while (slow != fast);
-  fast = nums[0];
-  while (slow != fast) {
-    slow = nums[slow];
-    fast = nums[fast];
-  }
-  return slow;
+        int i;
+        for(i=0;i<l.size()-1;i++)
+        {
+            while(l[i]-1!=i && l[i]!=l[l[i]-1])
+            {
+                swap(l[i],l[l[i]-1]);
+            }
+            if(l[i]==l[l[i]-1] && l[i]-1!=i)
+            {
+                return(l[i]);
+            }
+        }
+        return(l[l.size()-1]);
+  //       int slow = nums[0];
+  // int fast = nums[0];
+  // do {
+  //   slow = nums[slow];
+  //   fast = nums[nums[fast]];
+  // } while (slow != fast);
+  // fast = nums[0];
+  // while (slow != fast) {
+  //   slow = nums[slow];
+  //   fast = nums[fast];
+  // }
+  // return slow;
     }
 };
